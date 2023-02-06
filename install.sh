ba6231736a7fb64029c73fb621733fb3f621f5ab
lipengfei <lipengfei_mail@126.com>
2023-02-06 13:42:08 +0800

    add install.sh script

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-source /zfs/pengfeili/software/anaconda3/bin/activate openmm
-python setup.py build_ext -I/zfs/pengfeili/software/anaconda3/envs/openmm/include/openbabel3/ -L/zfs/pengfeili/software/anaconda3/envs/openmm/lib
+source /data/users/lipengfei/software/anaconda3/2021.11/bin/activate openmm
+python setup.py build_ext -I/data/users/lipengfei/software/openbabel/3.1.1/include/openbabel3 -L/data/users/lipengfei/software/openbabel/3.1.1/lib
 python setup.py install
